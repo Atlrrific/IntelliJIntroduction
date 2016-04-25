0d99bd43a14b4febd86486d980328693c0d8c0cd
Atl Arredondo <atlsaber@gmail.com>
2016-04-25 10:39:57 -0700

    Le MEssage

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -13,12 +13,12 @@ public class Celsius extends Temperature {
     @Override
     public Temperature toCelsius() {
 
-        return null;
+        return this;
     }
 
     @Override
     public Temperature toFahrenheit() {
-        return null;
+        return new Fahrenheit((float)(this.getValue()*1.8 + 32));
 
     }
 
@@ -28,6 +28,6 @@ public class Celsius extends Temperature {
     public String toString()
         {
             // TODO: Complete this method
-            return "";
+            return "" + this.getValue() + " C";
         }
 }
